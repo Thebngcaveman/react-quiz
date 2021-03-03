1266e16ea2429d5c968375ee7894184ac8010766
Suppanut Sittimakarn <Thebngcaveman@gmail.com>
2021-03-03 21:57:55 +0700

    first commit

---
+++ src/QuizData.js
@@ -0,0 +1,62 @@
+export const QuizData = [
+  {
+    id: 0,
+    question: `Which countries have bombed the American at Pearl Harbour?`,
+    options: [`Empire of Japan`, `German`, `Soviet`, `France`],
+    answer: `Empire of Japan`,
+  },
+  {
+    id: 1,
+    question: `Who is the only one that can solve Nazi encripted code?`,
+    options: [`Rommel`, `Herman Goerring`, `Winston Churchill`, `Alan Turing`],
+    answer: `Alan Turing`,
+  },
+  {
+    id: 2,
+    question: `Which countries Hitler didn't invade?`,
+    options: [`Poland`, `Italy`, `France`, `Czechoslovakia`],
+    answer: `Italy`,
+  },
+  {
+    id: 3,
+    question: `Who remain neutral ground when it comes to a war`,
+    options: [`Switzerland`, `Turkey`, `Spain`, `England`],
+    answer: `Switzerland`,
+  },
+  {
+    id: 4,
+    question: `Who is supposed to be successor after Lenin died`,
+    options: [`Stalin`, `Gorbachev`, `Khrushchev`, `Trotsky`],
+    answer: `Trotsky`,
+  },
+  {
+    id: 5,
+    question: `What year is the end of World War 2`,
+    options: [`1935`, `1938`, `1941`, `1945`],
+    answer: `1945`,
+  },
+  {
+    id: 6,
+    question: `What is most bloodiest place in WW2 that leads to Nazi's greatest defeat`,
+    options: [`Leningrad`, `Stalingrad`, `Moscow`, `Nagasaki`],
+    answer: `Stalingrad`,
+  },
+  {
+    id: 7,
+    question: `Which one of these provinces did American drop the bomb`,
+    options: [`Nagasaki`, `Bangkok`, `Tokyo`, `Normandy`],
+    answer: `Nagasaki`,
+  },
+  {
+    id: 8,
+    question: `Which countries have lost the most ammount of people in WW2`,
+    options: [`Soviet`, `German`, `Italy`, `England`],
+    answer: `Soviet`,
+  },
+  {
+    id: 9,
+    question: `Who said the word "We have defeated the wrong enemy"`,
+    options: [`Roosevelt`, `Hirohito`, `George S. Patton`, `Rommel`],
+    answer: `George S. Patton`,
+  },
+];
